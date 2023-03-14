4feeeb892a53360744f42dd4abfff1ccae318a4d
S.Tsar <16030558+Tsar36@users.noreply.github.com>
2023-03-14 15:15:52 +0200

    UPD

--- vars.tf
+++ vars.tf
@@ -3,3 +3,11 @@ variable "AWS_SECRET_KEY' {}
 variable "AWS_REGION' {
     default = "eu-west-1"
 }
+variable "AMIS" {
+    type = "map"
+    default = {
+        us-east- "ami-13be557e"
+        us-west-2 = “ami-06b94666"
+        eu-west-1 = "ami-0d729a60"
+    }
+}
